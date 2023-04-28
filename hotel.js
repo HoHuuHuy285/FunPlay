7b5c311e11dce43047cd234efba77c86329b1872
Huy <118819624+HoHuuHuy285@users.noreply.github.com>
2023-04-28 08:32:30 +0700

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,64 @@
+console.log('file loaded!');
+// create time
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  let session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+  document.getElementById('txt').innerHTML = h + ":" + m + ":" + s + " " + session;
+  setTimeout(startTime, 1000);
+}
+// create date
+// create map by api
+// https://console.cloud.google.com  but fixed it all/
+(g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })({
+  key: "AIzaSyA4jXioOnAqSCZxy9y1Zl8udMK6EkvmDYM",
+});
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 16.07104362794702, lng: 108.22028342511129 },
+    zoom: 100,
+  });
+}
+initMap();
+
+
+// button 
+document.querySelectorAll('.detail').forEach(element => {
+ element.addEventListener('click',function(){
+  alert('ERROR');
+});
+});
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
+
+
+var date = new Date();
+document.getElementById("year").innerHTML = date;
+
+function Copyright() {
+    var year = date.getFullYear();
+    document.getElementById("year").innerHTML = year;
+}
